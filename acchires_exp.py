4f8039c17e9d28c3998d36d37537a5ea9d299ba9
ggqshr <ggq18663278150@sina.com>
2020-01-15 22:24:53 +0800

    更改类型

--- acchires_exp.py
+++ acchires_exp.py
@@ -18,7 +18,7 @@ def find_accHires_id():
     accStartYearRange = [2002, 2009]
     for item in cl_updated_aft.find(no_cursor_timeout=False):
         pid, accStartDate = item.get("person_id"), item.get("accStartDate")
-        if accStartDate >= accStartYearRange[0] and accStartDate <= accStartYearRange[1]:
+        if int(accStartDate) >= accStartYearRange[0] and int(accStartDate) <= accStartYearRange[1]:
             person_id.add(pid)
     return person_id
 
